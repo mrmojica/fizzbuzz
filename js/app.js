72c96ca8dfc67b796ff40abb0973e9afc9cd7d99
mrmojica <mrmojica510@gmail.com>
2016-06-29 12:56:52 -0700

    fizzbuzz challenge

---
+++ js/app.js
@@ -0,0 +1,26 @@
+function fizzBuzz(){
+
+
+for (var i =1; i <= 100; i++){
+	if((i%3)===0){	
+		document.write('<p>' + 'fizz' + '</p>');
+	}else if((i%5)===0){
+		document.write('<p>' + 'buzz' + '</p>');
+	}else{
+		document.write('<p>' + i + '</p>');
+	}
+
+}
+
+
+};
+
+
+fizzBuzz();
+
+
+
+
+
+
+
